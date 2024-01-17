e2fafba81698d6a60d75ece914f9ec726a0be48c
Jose Lima <jalimag0206@gmail.com>
2024-01-17 03:47:27 -0300

    feat: add deleteTaskController

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import fastify from "fastify";
-import { createTaskController, updateTaskController } from "./controllers/";
+import {
+    createTaskController,
+    deleteTaskController,
+    updateTaskController,
+} from "./controllers/";
 
 export const app = fastify();
 
 app.post("/tasks", createTaskController);
 app.patch("/tasks/:taskId", updateTaskController);
+app.delete("/tasks/:taskId", deleteTaskController);
---
+++ src/controllers/delete-task.ts
@@ -0,0 +1,34 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import validator from "validator";
+
+import { makeDeleteTaskUseCase } from "@/use-cases/factories";
+
+export const deleteTaskController = async (
+    request: FastifyRequest,
+    reply: FastifyReply
+) => {
+    const taskBodySchema = z.object({
+        taskId: z.string(),
+    });
+
+    const { taskId } = taskBodySchema.parse(request.params);
+
+    try {
+        const deleteTaskUseCase = makeDeleteTaskUseCase();
+
+        const isIdValid = validator.isUUID(taskId);
+
+        if (!isIdValid) {
+            return reply.status(404).send({ message: "Id is not valid" });
+        }
+
+        await deleteTaskUseCase.execute({
+            taskId,
+        });
+
+        return reply.status(200).send;
+    } catch (error) {
+        reply.status(404).send({ message: "Id is not found" });
+    }
+};
